fbb31f60494a3b8b75b010f8c8db15917c6f446e
Allan <Allanprakoso7@gmail.com>
2022-01-10 17:12:19 +0700

    add creator and status recipe

--- src/services/postgres/RecipesService.js
+++ src/services/postgres/RecipesService.js
@@ -149,14 +149,9 @@ class RecipesService {
         });
 
         const query = {
-            text: 'SELECT recipes.id, name, url_image, cooking_time, serving, created_at, updated_at, status, creators.username as creator FROM recipes INNER JOIN creators ON recipes.creator_id = creators.id WHERE recipes.id = $1',
+            text: 'SELECT recipes.id, recipes.name, recipes.url_image, cooking_time, serving, created_at, updated_at, status, creators.username as creator FROM recipes INNER JOIN creators ON recipes.creator_id = creators.id WHERE recipes.id = $1',
             values: [id],
         };
-        try {
-            await this._pool.query(query);
-        } catch (e) {
-            console.log(e)
-        }
         const result = await this._pool.query(query);
         if (result.rows.length === 0) {
             throw new NotFoundError('Recipe not found');
